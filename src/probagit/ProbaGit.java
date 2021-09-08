6234b6cd5146faf84d9b995cb7e8edab196a51f5
pupil <pupil@ivkhk.ee>
2021-09-08 13:31:10 +0300

    первое изменение

--- src/probagit/ProbaGit.java
+++ src/probagit/ProbaGit.java
@@ -16,6 +16,7 @@ public class ProbaGit {
      */
     public static void main(String[] args) {
         System.out.println("Начало разработки");
+        System.out.println("Первое изменение кода");
     }
     
 }
